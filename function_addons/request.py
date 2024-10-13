cf3a4456cefa17595b394d13f09a015f9635ec31
Victor <victor@bmlabs.eu>
2024-10-13 23:52:27 +0200

    Remove testing type_check code

--- function_addons/request.py
+++ function_addons/request.py
@@ -26,16 +26,3 @@ class Context(TypedDict):
     function_version: str
     namespace: str
     request_id: str
-
-def type_check(func: Callable) -> Callable:
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        annotations = func.__annotations__
-        for arg_name, arg_value in kwargs.items():
-            if arg_name in annotations:
-                expected_type = annotations[arg_name]
-                if not isinstance(arg_value, expected_type):
-                    raise TypeError(f"Argument '{arg_name}' must be of type {expected_type.__name__}, got {type(arg_value).__name__}")
-        return func(*args, **kwargs)
-    return wrapper
-
